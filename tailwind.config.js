7fcf6fff69e993c12ed9ddbdeeeb3c7e95e9008a
Mash Codee <codeemash@gmail.com>
2022-03-17 14:51:54 +0530

    build(design): add colors and fonts variables in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Montserrat", "sans-serif"],
+        body: ["Lato", "sans-serif"],
+      },
+      colors: {
+        "primary": "#0D5F9A",
+        "secondary-text": "#858585",
+        "link": "#346BD5",
+        "field-darker": "#EAEAEA",
+        "background": "#F5F5F5",
+        "light-grey": "#B0B0B0",
+        "icon": "#999999",
+        "light-red": "#E9A0A0",
+        "light-green": "#9BDD7C"
+      }
+    },
   },
   plugins: [],
 }
