72f7e6b8b0ab253dc4a9a5e22a748252137aa26f
Mash Codee <codeemash@gmail.com>
2022-03-17 14:35:16 +0530

    build: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next"
+import Head from "next/head"
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Blue Sky Analytics - Dashboard</title>
-        <meta name="description" content="Dashboard UI Development - Blue Sky Analytics Job Assignment" />
-        <link rel="icon" href="/favicon.webp" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Blue Sky Analytics - Dashboard</title>
+				<meta
+					name="description"
+					content="Dashboard UI Development - Blue Sky Analytics Job Assignment"
+				/>
+				<link rel="icon" href="/favicon.webp" />
+			</Head>
 
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </>
-  )
+			<main className="p-12">
+				<h1 className="text-4xl">Hello World</h1>
+			</main>
+		</>
+	)
 }
 
 export default Home
