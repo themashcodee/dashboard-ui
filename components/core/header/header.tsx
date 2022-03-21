10beb94aed9c46beeb66a40390d21f362376351c
Mash Codee <codeemash@gmail.com>
2022-03-21 10:26:27 +0530

    feat(header): add header component

---
+++ components/core/header/header.tsx
@@ -0,0 +1,39 @@
+import React, { ComponentPropsWithoutRef } from "react"
+import { Component } from "types/common"
+import { SearchInput } from "components/core"
+import { NotificationIcon } from "components/icons"
+import Image from "next/image"
+import { useSession } from "next-auth/react"
+import UserImage from "assets/user.jpg"
+
+type P = ComponentPropsWithoutRef<"header"> & {
+	tabName: string
+}
+
+export const Header: Component<P> = ({ tabName, className, ...props }) => {
+	const session = useSession()
+
+	return (
+		<header
+			className={`flex items-center py-4 gap-4 justify-between ${className}`}
+			{...props}
+		>
+			<h1 className="font-semibold text-2xl">{tabName}</h1>
+
+			<nav className="flex items-center gap-6">
+				<SearchInput />
+				<NotificationIcon />
+
+				<div className="rounded-full h-10 w-10 overflow-hidden bg-red-200">
+					<Image
+						src={session.data?.user?.image ?? UserImage}
+						width={40}
+						height={40}
+						alt="profile picture"
+						objectFit="cover"
+					/>
+				</div>
+			</nav>
+		</header>
+	)
+}
